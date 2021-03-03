1666b67da0cbd83af99f8648f529976f5925c4ec
DaCoPro <comerbdavid@gmail.com>
2021-03-03 11:46:01 -0800

    Correctly POST & receive token from API.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect, useCallback } from 'react';
 
-function App() {
+export default function App() {
+
+  const accessToken = {"username":"test", "password":"test123!"};
+  const apiUrl = 'https://messaging-test.bixly.com'
+
+  axios.interceptors.request.use(
+    config => {
+      config.headers.Authorization = `Token ${accessToken}`;
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  );
+  console.log(accessToken);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const result = await axios({
+        method: 'post',
+        url: `${apiUrl}/api-token-auth/`,
+        headers: {},
+        data: {
+          "username":"test", "password":"test123!"
+        }
+      })
+      
+      // .get(`${apiUrl}/messages/`)
+      
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {
+        fetchData();
+      }}
+      >Fetch Data</button>
     </div>
   );
 }
 
-export default App;
+
